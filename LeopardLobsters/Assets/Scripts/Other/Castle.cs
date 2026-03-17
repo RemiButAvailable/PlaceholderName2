d1350a50c5b50a45c555f7685eaf5acf138bafeb
Balbert-Bam <albert.tan@digipen.edu>
2026-03-17 14:55:37 -0700

    menu stuff + temp assets and Ui set up

---
+++ LeopardLobsters/Assets/Scripts/Other/Castle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Castle : MonoBehaviour
+{
+    public int peopleAtCastle = 0;
+    public int peopleTotal = 0;
+    public int money = 0;
+    public int happiness = 1; //percentage
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

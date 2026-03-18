6d25d5e81b487b3f79849c9ea67608e068cf6996
Balbert-Bam <albert.tan@digipen.edu>
2026-03-18 14:06:24 -0700

    gaming

--- LeopardLobsters/Assets/Scripts/Other/Castle.cs
+++ LeopardLobsters/Assets/Scripts/Other/Castle.cs
@@ -4,8 +4,6 @@ public class Castle : MonoBehaviour
 {
     public int peopleAtCastle = 0;
     public int peopleTotal = 0;
-    public int money = 0;
-    public int happiness = 1; //percentage
 
     public void endOfWave() {
         //add coins based on people

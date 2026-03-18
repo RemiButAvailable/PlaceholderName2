6390d7517277187907a78eca8f31671a8cc6ccc8
Balbert-Bam <albert.tan@digipen.edu>
2026-03-18 14:56:37 -0700

    castle stuff

--- LeopardLobsters/Assets/Scripts/Other/Castle.cs
+++ LeopardLobsters/Assets/Scripts/Other/Castle.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Castle : MonoBehaviour
 {
-    public int peopleAtCastle = 0;
-    public int peopleTotal = 0;
+    public int peopleAtCastle = 5; //starting amount of people
+    int peopleTotal;
+    public int moneyPerPerson = 5;
+    public MoneyManagerScript moneyManager; //instantiate in inspector
 
+
+    private void Awake()
+    {
+        peopleTotal = peopleAtCastle;
+    }
+
+    //adds money based on population
     public void endOfWave() {
-        //add coins based on people
-
-        //idfk
+        moneyManager.moneyNum += peopleAtCastle * moneyPerPerson;
+        //DO: add money sfx vfx
     }
 
     //when knight reaches the castle
     public void enemyArrived() {
         peopleAtCastle--;
         if (peopleAtCastle < 0) {
-            //lose stuff
+            //DO: lose game stuff 
+            SceneManager.LoadScene("PeopleLoseScreen");
         }
     }
 
@@ -23,24 +34,27 @@ public class Castle : MonoBehaviour
     public bool inWave = false;
     float timer = 0;
     public int timerMax = 10;
+    
     public int minPeopleNeeded = 2; //amount of people that are required to be at castle to make more people
     public int maxPeopleDecrease = 10; //max amount of people that increase speed of timer
     public float percentPerPerson = 1.25f; //the percent multiplied that reduce time for timer
+
+    public Image progressBar; //instantiate in inspector
 
     private void FixedUpdate()
     {
         if (inWave && peopleAtCastle>=minPeopleNeeded)
         {
-            //DO: change progress bar
+            progressBar.fillAmount = timer / timerMax;
 
             //increase time passed based on people at castle
-            timer -= Time.deltaTime * Mathf.Pow(percentPerPerson, 
+            timer += Time.deltaTime * Mathf.Pow(percentPerPerson, 
                 Mathf.Min(peopleAtCastle,maxPeopleDecrease)-minPeopleNeeded);
 
-            //increases people when timer is done
-            if (timer < 0)
+            //increases people when timer is done BROKEN DO SOMETHING
+            if (timer >= timerMax)
             {
-                timer = timerMax;
+                timer = 0;
                 //DO: people added SFX VFX
 
                 peopleAtCastle++;

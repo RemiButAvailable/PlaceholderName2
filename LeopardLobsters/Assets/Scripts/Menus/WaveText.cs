70dccb53c1fd31582adf2278a152daac84162859
Victoria T. <alexatroshkov@gmail.com>
2026-04-20 14:34:26 -0700

    new...

--- LeopardLobsters/Assets/Scripts/Menus/WaveText.cs
+++ LeopardLobsters/Assets/Scripts/Menus/WaveText.cs
@@ -5,7 +5,7 @@ public class WaveText : MonoBehaviour
 {
     [SerializeField] WaveCode waveCode;
 
-    TextMeshProUGUI wafer;
+    public TextMeshProUGUI wafer;
     private void Start()
     {
         wafer.text = "Killed at wave " + waveCode.WaveNum;

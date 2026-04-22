e5cd5783eef79874331f5dc1a3866e9927940b2a
TheCodingKnight-dude <jones.d@digipen.edu>
2026-04-22 13:04:52 -0700

    Hit sound on death

--- LeopardLobsters/Assets/Scripts/Enemies/KnightScript.cs
+++ LeopardLobsters/Assets/Scripts/Enemies/KnightScript.cs
@@ -62,8 +62,10 @@ public class KnightScript : MonoBehaviour, IComparable<KnightScript>
     [SerializeField] AudioSource hurtSound;
     [SerializeField] AudioSource ArrowHitSound;
     [SerializeField] AudioResource deathSound;
-    
+    [SerializeField] AudioResource hitDeathSound;
+
     [SerializeField] AudioPlayer aPlayerPrefab;
+    [SerializeField] AudioPlayer aHitSoundPrefab;
     [SerializeField] float deathSoundVolume = .5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -115,7 +117,9 @@ public class KnightScript : MonoBehaviour, IComparable<KnightScript>
             //move.Play("BarbIAN_guy_DEATH");
             //sounds
             AudioPlayer aPlayer = Instantiate(aPlayerPrefab);
-            aPlayer.playClip(transform.position, deathSound, deathSoundVolume);
+            AudioPlayer aHitSound = Instantiate(aHitSoundPrefab);
+            aPlayer.playClip(transform.position, hitDeathSound, deathSoundVolume);
+            aHitSoundPrefab.playClip(transform.position, deathSound, deathSoundVolume);
            
             //if (inhabitedTowerZone != null)
             //inhabitedTowerZone.queue.Remove(this.gameObject);

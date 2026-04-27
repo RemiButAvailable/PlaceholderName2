316dbd5d872e280dbc41187ed8353d4777ec74ee
Victoria T. <alexatroshkov@gmail.com>
2026-04-27 13:30:42 -0700

    change

--- LeopardLobsters/Assets/Scripts/Menus/WaveText.cs
+++ LeopardLobsters/Assets/Scripts/Menus/WaveText.cs
@@ -1,3 +1,11 @@
+/**********
+ * Forgor to document what actual day so 4/27/26
+ * Author: Victoria T.
+ * 
+ * 
+ * Description: Just something to document at what wave the player died, in case
+ * they wanted to know.
+ */
 using UnityEngine;
 using TMPro;
 

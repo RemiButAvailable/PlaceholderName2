5cbef7a72d12fcab105e89a40ef3c4beab9e99a6
TheCodingKnight-dude <jones.d@digipen.edu>
2026-04-27 12:46:35 -0700

    Commented in button sound script

--- LeopardLobsters/Assets/Scripts/Audio/ButtonSoundScript.cs
+++ LeopardLobsters/Assets/Scripts/Audio/ButtonSoundScript.cs
@@ -1,11 +1,23 @@
+/**********************************************************************
+ * File Name: ButtonSoundScript.cs
+ * 
+ * Author: Dante Jones
+ * Digipen Email: jones.d@digipen.edu
+ * 
+ * Description: This script plays a sound when
+ * a button is pressed.
+ * 
+ * ********************************************************************/
 using UnityEngine;
 
 public class ButtonSoundScript : MonoBehaviour
 {
+    //The sound when the button is pressed
     public AudioSource ButtonSound;
     //Plays audio
     public void Play()
     {
+        //The button sound plays
         ButtonSound.Play();
     }
 

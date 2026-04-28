3eb2633b98e10b9deecff6b6b6184f9b5ee46042
RemiButAvailable <deplaterfrankie@gmail.com>
2026-04-28 14:58:05 -0700

    i hate this so much

--- LeopardLobsters/Assets/Scripts/Towers/SoldierTower/SoldierTowerEnemyDetection.cs
+++ LeopardLobsters/Assets/Scripts/Towers/SoldierTower/SoldierTowerEnemyDetection.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class SoldierTowerEnemyDetection : MonoBehaviour
 {
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "knight")
-    //    {
-    //        soliderTower.GetComponent<SoldierTowerScript>().AddEnemy(collision.gameObject);
-    //    }
-    //}
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "knight")
-    //    {
-    //        soliderTower.GetComponent<SoldierTowerScript>().RemoveEnemy(collision.gameObject);
-    //    }
-    //}
+    public GameObject soldierTower;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "knight")
+        {
+            soldierTower.GetComponent<SoldierTowerScript>().AddEnemy(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "knight")
+        {
+            soldierTower.GetComponent<SoldierTowerScript>().RemoveEnemy(collision.gameObject);
+        }
+    }
 }
